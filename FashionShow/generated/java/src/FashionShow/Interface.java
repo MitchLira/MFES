bfdc410d4cf397647dac1dfd74a98e2b8d1aa1f3
Acer-PC\Acer <up201404022@fe.up.pt>
2018-01-02 20:35:11 +0000

    Interface Increment #2

--- FashionShow/generated/java/src/FashionShow/Interface.java
+++ FashionShow/generated/java/src/FashionShow/Interface.java
@@ -2,8 +2,14 @@ package FashionShow;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
+
+import FashionShow.Platform.Date;
+import FashionShow.quotes.FemaleQuote;
+import FashionShow.quotes.MaleQuote;
 
 public class Interface {
 	
@@ -39,7 +45,7 @@ public class Interface {
 					UsersMainMenu();
 					break;
 				case 2:	
-					//TODO
+					EventsMainMenu();
 					break;
 				case 3:
 					//TODO
@@ -55,6 +61,428 @@ public class Interface {
 		}
 	}
 	
+//----------------------------------------------Events--------------------------------------------------	
+	
+	public static void EventsMainMenu()
+	{
+		ArrayList<Event> foundEvents;
+		while(true)
+		{
+			System.out.println("#==================Events Menu==================#");
+			System.out.println("Choose an option: \n");
+			System.out.println("1- Show All Events;");
+			System.out.println("2- Event Operations;");
+			System.out.println("3- Create Event");
+			System.out.println("4- Delete Event;");
+			System.out.println("0- Return;");
+			System.out.println("#==============================================#");
+			int option = intReader(0,4);
+			switch (option)
+			{
+				case 0:
+					return;
+				case 1:
+					viewAllEvents();
+					break;
+				case 2:	
+					foundEvents = searchPlatformEvent();
+					if(foundEvents.size()>0)
+					{
+						System.out.println("Choose a event to operate: (Index, 0 to Cancel;)");
+						int index = intReader(0,foundEvents.size());
+						if(index == 0)
+							break;
+						Event chosenEvent = foundEvents.get(index-1);
+						operateEventMenu(chosenEvent);
+					}
+					break;
+				case 3:
+					createEvent();
+					break;
+				case 4:
+					foundEvents = searchPlatformEvent();
+					deleteEvent(foundEvents);
+					break;
+				
+			}
+		}
+	}
+	
+	private static void operateEventMenu(Event event) {
+		while(true)
+		{
+			ArrayList<Designer> foundDesigners;
+			System.out.println("#==================Event: "+event.name+"==================#");
+			System.out.println("Choose an option: \n");
+			System.out.println("1- View Event Info;");
+			System.out.println("2- Add Designer;");
+			if(event instanceof Runway)
+			{
+				System.out.println("3- Add Model;");
+				System.out.println("4- Add Item;");
+			}
+			System.out.println("0- Return;");
+			System.out.println("#========================================#");
+			if(event instanceof Runway)
+			{
+				int option = intReader(0,4);
+				switch (option)
+				{
+					case 0:
+						return;
+					case 1:
+						viewEventInfo(event);
+						break;
+					case 2:
+						foundDesigners = searchPlatformDesigner();
+						if(foundDesigners.size()>0)
+						{
+							System.out.println("Choose a desginer to add: (Index, 0 to Cancel;)");
+							int index = intReader(0,foundDesigners.size());
+							if(index==0)
+								break;
+							Designer chosenDesigner = foundDesigners.get(index-1);
+							event.addDesigner(chosenDesigner);
+							System.out.println("Designer added successfully");
+						}
+						
+						break;
+					case 3:	
+						createModel(event);
+						break;
+					case 4:
+						addItemToEvent(event);
+						break;
+				}
+			}
+			else
+			{
+				int option = intReader(0,2);
+				switch (option)
+				{
+					case 0:
+						return;
+					case 1:
+						viewEventInfo(event);
+						break;
+					case 2:
+						foundDesigners = searchPlatformDesigner();
+						if(foundDesigners.size()>0)
+						{
+							System.out.println("Choose a desginer to add: (Index, 0 to Cancel;)");
+							int index = intReader(0,foundDesigners.size());
+							if(index==0)
+								break;
+							Designer chosenDesigner = foundDesigners.get(index-1);
+							event.addDesigner(chosenDesigner);
+							System.out.println("Designer added successfully");
+						}
+						break;
+					
+				}
+			}
+		}
+	}
+
+	private static void addItemToEvent(Event event) {
+		System.out.println("#==================Add Item==================#");
+		if(event.designers.size()==0)
+		{
+			System.out.println("This Event has no Designers yet. Press Enter to return:");
+			System.out.println("#============================================#");
+			scanner.nextLine();
+		}
+		else
+		{	
+			int counter = 1;
+			Iterator it = event.designers.iterator();
+			ArrayList<Map<Designer,Item>> allItems = new ArrayList<Map<Designer,Item>>();
+			while(it.hasNext())
+			{
+				Designer designer = (Designer) it.next();
+				if(designer.items.size()>0)
+				{
+					System.out.println("Designer: "+designer.name);
+					Iterator it2 = designer.items.iterator();
+					while(it2.hasNext())
+					{
+						Item item = (Item)it2.next();
+						System.out.println("        -> "+counter+"- Item: "+item.name+ " | Ref: " + item.reference);
+						counter++;
+						Map<Designer,Item> map = new HashMap<Designer,Item>();
+						map.put(designer, item);
+						allItems.add(map);
+					}
+				}
+			}
+			System.out.println("Choose a item to add to the event. (Index, 0 to cancel)");
+			int index = intReader(0,allItems.size());
+			if(index == 0)
+				return;
+			((Runway)event).addDesignerItem((Designer) allItems.get(index-1).keySet().toArray()[0], allItems.get(index-1).get(allItems.get(index-1).keySet().toArray()[0]));
+			System.out.println("Item added successfully!");
+		}
+		
+	}
+
+	private static void createModel(Event event) {
+		String name;
+		int age;
+		float height;
+		Object gender;
+		String nationality;
+		
+			System.out.println("What's the model name?");
+			name=scanner.nextLine();
+			System.out.println("What's the model age?");
+			age= intReader(10,65);
+			System.out.println("What's the model gender?");
+			System.out.println(" --> 1- Female");
+			System.out.println(" --> 2- Male");
+			int genderChoice = intReader(1,2);
+			if(genderChoice == 1)
+				gender = new FemaleQuote();
+			else
+				gender = new MaleQuote();
+			System.out.println("What's the model height?");
+			height = Float.parseFloat(scanner.nextLine());
+			System.out.println("What's the model nationality?");
+			nationality = scanner.nextLine();
+			
+		Model model = new Model(name,age,height,nationality,gender);
+		model.addShow(event);
+		((Runway)event).addModel(model);
+		
+	}
+
+	private static ArrayList<Designer> searchPlatformDesigner() {
+		ArrayList<Designer> foundDesigners = new ArrayList<Designer>();
+		System.out.println("Search for an Designer: (Name)");
+		String designerString = scanner.nextLine();
+		Iterator it = platform.designers.iterator();
+		while(it.hasNext())
+		{
+			Designer designer = (Designer)it.next();
+			if(designer.name.contains(designerString))
+			{
+				foundDesigners.add(designer);
+			}
+		}
+		System.out.println("#===========Showing Designers==========#");
+		if(foundDesigners.size()==0)
+		{
+			System.out.println("-> No Designers found press Enter to back;");
+			System.out.println("#========================================#");
+			scanner.nextLine();
+		}
+		else
+		{
+			for (int i = 0; i < foundDesigners.size(); i++)
+			{
+				Designer designer = foundDesigners.get(i);
+				System.out.println("-> "+(i+1)+". Nome: "+ designer.name);
+			}
+			System.out.println("#========================================#");
+		}
+		return foundDesigners;
+		
+	}
+
+	private static void viewEventInfo(Event event) {
+		System.out.println("#==================Event==================#");
+		System.out.println("Category: "+event.getClass().getCanonicalName());
+		System.out.println("Name: "+ event.name);
+		System.out.println("Theme: "+ event.theme);
+		System.out.println("Place: "+ event.place);
+		System.out.println("Price: "+ event.price + " €");
+		System.out.println("Audience: "+ event.audience.size()+ "/" +event.maxSpectators);
+		System.out.println("Designers:");
+		Iterator it = event.designers.iterator();
+		if(event.designers.size()==0)
+		{
+			System.out.println("        ->This event has no designers yet.");
+		}
+		else{
+			while(it.hasNext())
+			{
+				Designer designer = (Designer) it.next();
+				System.out.println("        ->Designer: "+designer.name+"");
+			}
+		}
+		if(event instanceof Runway)
+		{
+			System.out.println("Models:");
+			it = ((Runway)event).models.iterator();
+			if(((Runway)event).models.size()==0)
+			{
+				System.out.println("        ->This event has no models yet.");
+			}
+			else{
+				while(it.hasNext())
+				{
+					Model model = (Model) it.next();
+					System.out.println("        ->Model: "+model.name);
+				}
+			}
+			System.out.println("Items:");
+			it = ((Runway)event).showItems.iterator();
+			if(((Runway)event).showItems.size()==0)
+			{
+				System.out.println("        ->This event has no items yet.");
+			}
+			else{
+				while(it.hasNext())
+				{
+					Item item = (Item) it.next();
+					System.out.println("        ->Item: "+item.name+ " | Price: "+ item.price + "€");
+				}
+			}
+		}
+		System.out.println("#========================================#");
+		System.out.println("Press Enter to go back:");
+		scanner.nextLine();
+		
+	}
+
+	private static ArrayList<Event> searchPlatformEvent() {
+		ArrayList<Event> foundEvents = new ArrayList<Event>();
+		System.out.println("Search for an Event: (Name or Theme)");
+		String eventString = scanner.nextLine();
+		Iterator it = platform.events.iterator();
+		
+		while(it.hasNext())
+		{
+			Event event = (Event)it.next();
+			if(event.name.contains(eventString) || event.theme.contains(eventString))
+			{
+				foundEvents.add(event);
+			}
+		}
+		System.out.println("#===========Showing Events==========#");
+		if(foundEvents.size()==0)
+		{
+			System.out.println("-> No Events found press Enter to back;");
+			System.out.println("#========================================#");
+			scanner.nextLine();
+		}
+		else
+		{
+			for (int i = 0; i < foundEvents.size(); i++)
+			{
+				Event event = foundEvents.get(i);
+				System.out.println("-> "+(i+1)+". Nome: "+ event.name+" | Theme: "+ event.theme +
+						" | Place: "+ event.place + "| Price: " + event.price +" €;");
+			}
+			System.out.println("#========================================#");
+		}
+		return foundEvents;
+	}
+
+	private static void deleteEvent(ArrayList<Event> foundEvents) {
+		if(foundEvents.size()>0)
+		{
+			System.out.println("Wich event do you want to delete?(Index) 0 to go back :");
+			int index = intReader(0,foundEvents.size()+1);
+			if(index == 0)
+			{
+				return;
+			}
+			else
+			{
+				Event chosenEvent = foundEvents.get(index-1);
+				Iterator it = chosenEvent.audience.iterator();
+				while(it.hasNext())
+				{
+					User user = (User)it.next();
+					chosenEvent.refundUser(user);
+				}
+				platform.removeEvent(chosenEvent);
+				System.out.println("Event deleted Successfully!");
+			}
+			
+		}
+	}
+
+	private static void createEvent() {
+		
+		String name;
+		String theme;
+		String place;
+		Date date;
+		float price;
+		int maxSpectators;
+
+			System.out.println("What's the Event category? (Index, 0 to Cancel)");
+			System.out.println("-> 1- Runway");
+			System.out.println("-> 2- Presentation");
+			System.out.println("-> 3- Primping Session");
+			int category = intReader(0,3);
+			if(category == 0)
+				return;
+			System.out.println("What's the Event name?");
+			name=scanner.nextLine();
+			System.out.println("What's the Event theme?");
+			theme=scanner.nextLine();
+			System.out.println("What's the Event place?");
+			place=scanner.nextLine();
+			System.out.println("What's the Event date?");
+			System.out.println("Day:");
+			int day = intReader(1,30);
+			System.out.println("Month (int):");
+			int month = intReader(1,12);
+			System.out.println("Year:");
+			int year = intReader(2000, 3000);
+			date = new Date(year,month,day);
+			System.out.println("What's the Event price?");
+			price =(float)intReader(0,Integer.MAX_VALUE);
+			System.out.println("What's the number of Max. participants?");
+			maxSpectators =intReader(1,Integer.MAX_VALUE);
+			
+		switch (category)
+		{
+			
+			case 1:
+				platform.addEvent(new Runway(name,date,place,theme,price,maxSpectators));
+				break;
+			case 2:	
+				platform.addEvent(new Presentation(name,date,place,theme,price,maxSpectators));
+				break;
+			case 3:
+				platform.addEvent(new PrimpingSession(name,date,place,theme,price,maxSpectators));
+				break;
+				
+		}
+		System.out.println("Event created successfully!");
+	}
+
+	private static void viewAllEvents() {
+		System.out.println("#================View All Events================#");
+		if(platform.events.size()!=0)
+		{
+			Iterator iterator = platform.events.iterator();
+			int counter = 1;
+			while(iterator.hasNext())
+			{
+				Event event = (Event)iterator.next();
+				System.out.println("->" + (counter) +". Type: "+ event.getClass().getCanonicalName() + " | Name: "+ event.name +
+									" | Theme: " + event.theme + " | Place: "+ event.place + " | Price: " + event.price + "€" + 
+									" | available spots: " + ((int)event.maxSpectators-event.audience.size()));
+				counter++;
+			}
+		}
+		else
+		{
+			System.out.println("There are no events at this moment.");
+		}
+		System.out.println("#==============================================#");
+		System.out.println("Press Enter to go back:");
+		scanner.nextLine();
+		
+	}
+
+//-----------------------------------------------Users--------------------------------------------------	
+	
+
 	public static void UsersMainMenu()
 	{
 		while(true)
@@ -121,7 +549,7 @@ public class Interface {
 					searchEvent(user); 
 					break;
 				case 5:
-					//TODO
+					refundEventTicket(user);
 					break;
 				case 6:
 					depositMoney(user);
@@ -130,6 +558,31 @@ public class Interface {
 		}
 	}
 	
+	private static void refundEventTicket(User user) {
+		System.out.println("#==================User Events==================#");
+		ArrayList<Event> userEvents = platform.getUserEvents(user);
+		if(userEvents.size()==0)
+		{
+			System.out.println("This user has no Event tickets");
+		}
+		else
+		{
+			for(int i = 0; i< userEvents.size(); i++)
+			{
+				Event event = userEvents.get(i);
+				System.out.println("-> "+(i+1)+". Nome: "+ event.name+" | Theme: "+ event.theme +
+						" | Place: "+ event.place + "| Price: " + event.price +" €;");
+			}
+		}
+		System.out.println("#========================================#");
+		System.out.println("Choose an event to be refunded: (Index, 0 to Cancel)");
+		int index = intReader(0,userEvents.size());
+		if(index==0)
+			return;
+		userEvents.get(index).refundUser(user);
+		
+	}
+
 	private static void showUserEvents(User user) {
 		System.out.println("#==================User Events==================#");
 		ArrayList<Event> userEvents = platform.getUserEvents(user);
